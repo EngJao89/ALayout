79e0874a227106f8a4e7957a20a5b1235dcc70d7
EngJao89 <eng_joao@outlook.com>
2024-11-12 20:47:31 -0300

    Initial Commit

---
+++ ALayoutTests/ALayoutTests.swift
@@ -0,0 +1,17 @@
+//
+//  ALayoutTests.swift
+//  ALayoutTests
+//
+//  Created by João Ricardo Martins Ribeiro on 12/11/24.
+//
+
+import Testing
+@testable import ALayout
+
+struct ALayoutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
